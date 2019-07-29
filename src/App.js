7e56a90a84b78e831c2b30732272b1f9ace0b844
kckartik17 <kckartik17@gmail.com>
2019-07-29 13:42:10 +0530

    alert added
    Signed-off-by: kckartik17 <kckartik17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 import User from "./components/users/Users";
 import Search from "./components/users/Search";
 import axios from "axios";
@@ -8,7 +9,8 @@ import axios from "axios";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   searchUsers = async text => {
@@ -24,18 +26,25 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 1000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className="App">
         <Navbar />
 
         <div className="container">
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <User users={users} loading={loading} />
         </div>
